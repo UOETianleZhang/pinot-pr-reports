df14c4381af52385d2d5a2c1dcfd35956747daf5
Matthew Jack <matvj250@gmail.com>
2025-06-18 20:23:49 -0700

    Update .pinot_regular_commit_compare.sh comments and messages to reflect new behavior

--- scripts/.pinot_regular_commit_compare.sh
+++ scripts/.pinot_regular_commit_compare.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# get count of commits from last hour. if there are no commits, exit the script
+# Get count of commits since beginning of last workflow run. 
+# Each workflow besides the very first one will look at all commits between the start time of the previous workflow
+# and the start time of this workflow.
+# If there are no commits within this timeframe, exit the script.
 echo "$1"
 commitcount=$(gh api repos/apache/pinot/commits --jq ".[] | select(.commit.committer.date >= \"$1\")" | wc -l)
 if [[ commitcount -eq 0 ]]; then
-  echo "There have been no commits in the last hour."
+  echo "There have been no commits since those processed by the last run of this workflow."
   exit 0
 fi
 
